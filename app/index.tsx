6bba122f7d13b1f82e0442a39ac743062fd43d91
AbubakerImran <abubakerimran4697@gmail.com>
2024-12-14 14:17:13 +0500

    main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
+import { router } from "expo-router";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StatusBar, StyleSheet, TouchableOpacity, View, Text, Image, Dimensions } from "react-native";
 
-const Signup = () => {
+const main = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Sign Up</Text>
+            <StatusBar backgroundColor='white' barStyle='dark-content' />
+            <View>
+                <Image source={require('../assets/images/main icon.jpg')} style={styles.image} resizeMode="center" />
+                <TouchableOpacity style={styles.loginbutton}><Text style={styles.logintext}>LOG IN</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.signupbutton} onPress={() => { router.navigate('/signup') }}><Text style={styles.signuptext}>SIGN UP</Text></TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 };
@@ -12,14 +18,47 @@ const Signup = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: 'white'
     },
-    text: {
-        fontSize: 30,
+    image: {
+        width: 300,
+        height: 200,
+        marginBottom: 40
+    },
+    loginbutton: {
+        backgroundColor: 'blue',
+        borderRadius: 7,
+        borderWidth: 3,
+        borderColor: 'blue',
+        paddingVertical: 10,
+        width: 250,
+        marginHorizontal: 'auto',
+        marginVertical: 10
+    },
+    signupbutton: {
+        backgroundColor: 'white',
+        borderRadius: 7,
+        borderWidth: 3,
+        borderColor: 'blue',
+        paddingVertical: 10,
+        width: 250,
+        marginHorizontal: 'auto',
+        marginVertical: 10
+    },
+    logintext: {
+        color: 'white',
         fontWeight: 'bold',
-        color: 'red',
+        fontSize: 20,
         textAlign: 'center'
     },
+    signuptext: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center'
+    }
 });
 
-export default Signup;
+export default main;
