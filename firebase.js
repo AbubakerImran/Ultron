6ca221fee447cb4c9df3824df8aea7ffb179f848
AbubakerImran <abubakerimran4697@gmail.com>
2024-12-14 18:38:40 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOzIlkzWT_cdltBjrOGszJIZKz5SkoC8A",
+    authDomain: "ultron-v1-0.firebaseapp.com",
+    projectId: "ultron-v1-0",
+    storageBucket: "ultron-v1-0.firebasestorage.app",
+    messagingSenderId: "254600426294",
+    appId: "1:254600426294:web:d54071f811025af562f997"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, setDoc, collection }
