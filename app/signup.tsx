d792e6623de600473f1a0d0e0463c09313106ed7
AbubakerImran <abubakerimran4697@gmail.com>
2024-12-14 18:38:51 +0500

    signup function

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
-import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, ScrollView } from "react-native"
+import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, ScrollView, ActivityIndicator } from "react-native";
+import { db } from "../firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const signUp = () => {
 
+    const [Name, setName] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
+    const [Loading, setLoading] = useState(false);
     const [Error, setError] = useState('');
+    const [success, setsuccess] = useState('red');
+
+    const handlesignup = async () => {
+        if (!Name || !Email || !Password) {
+            setError('All Fields are required');
+        } else {
+            try {
+                setError('');
+                setLoading(true);
+                const checkUser = doc(db, "users", Email);
+                const User = await getDoc(checkUser);
+                if (User.exists()) {
+                    setError("User already exist with this email");
+                    setsuccess("red");
+                } else {
+                    await setDoc(doc(db, "users", Email), { Name, Email, Password });
+                    setError('Account successfully created');
+                    setsuccess('green');
+                }
+            } catch (e) {
+                console.log(e);
+                setError('Error creating account. Try again later');
+            } finally {
+                setLoading(false);
+            }
+        };
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -13,11 +46,11 @@ const signUp = () => {
                     <Image source={require('../assets/images/signup icon.jpg')} style={styles.image} resizeMode="center" />
                     <Text style={styles.heading}>Hi!</Text>
                     <Text style={styles.heading2}>Create a new account</Text>
-                    <Text>{Error}</Text>
-                    <TextInput inputMode='text' placeholder="Full Name" style={styles.input} autoCapitalize="words" />
-                    <TextInput inputMode='email' placeholder="Enter your email" style={styles.input} autoCapitalize="none" />
-                    <TextInput inputMode='text' placeholder="Enter your password" secureTextEntry={true} style={styles.input} autoCapitalize="words" />
-                    <TouchableOpacity style={styles.button}><Text style={styles.buttontext}>SIGN UP</Text></TouchableOpacity>
+                    <Text style={{ textAlign: 'center', color: success }}>{Error}</Text>
+                    <TextInput inputMode='text' placeholder="Full Name" style={styles.input} autoCapitalize="words" value={Name} onChangeText={setName} />
+                    <TextInput inputMode='email' placeholder="Enter your email" style={styles.input} autoCapitalize="none" value={Email} onChangeText={setEmail} />
+                    <TextInput inputMode='text' placeholder="Enter your password" secureTextEntry={true} style={styles.input} autoCapitalize="none" value={Password} onChangeText={setPassword} />
+                    <TouchableOpacity style={styles.button} onPress={handlesignup}>{Loading ? <ActivityIndicator size={27} color='white' /> : <Text style={styles.buttontext}>SIGN UP</Text>}</TouchableOpacity>
                 </View>
             </ScrollView>
         </SafeAreaView >
